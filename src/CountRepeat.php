e0f1a9451024db805fc47a5a732f2c732cdcd9a8
Aundra Miller <miller.aundra@gmail.com>
2016-02-19 11:56:02 -0800

    fix bug for empty search and add bottom border to result declaration

--- src/CountRepeat.php
+++ src/CountRepeat.php
@@ -20,23 +20,30 @@
             $final_array = array();
             $count = 0;
         //Count function
-            foreach ($sentence_array as $index => $word) {
-                if ($sentence_array[$index] == $final_word) {
-                    if ($index == 0) {
-                        $sentence_array[$index] = ucfirst($sentence_array[$index]);
+            if ($sentence_array == array("")) {
+                $count = 0;
+                $return_sentence = implode($return_sentence);
+                array_push($final_array, $count, $return_sentence);
+                return $final_array;
+            } else {
+                foreach ($sentence_array as $index => $word) {
+                    if ($sentence_array[$index] == $final_word) {
+                        if ($index == 0) {
+                            $sentence_array[$index] = ucfirst($sentence_array[$index]);
+                        }
+                            $count += 1;
+                            array_push($return_sentence, "<span class='match'>$sentence_array[$index]</span>");
+                    } else {
+                        if ($index == 0) {
+                            $sentence_array[$index] = ucfirst($sentence_array[$index]);
+                        }
+                            array_push($return_sentence, $sentence_array[$index]);
                     }
-                        $count += 1;
-                        array_push($return_sentence, "<span class='match'>$sentence_array[$index]</span>");
-                } else {
-                    if ($index == 0) {
-                        $sentence_array[$index] = ucfirst($sentence_array[$index]);
-                    }
-                        array_push($return_sentence, $sentence_array[$index]);
                 }
+                $return_sentence = implode(" ", $return_sentence);
+                array_push($final_array, $count, $return_sentence);
+                return $final_array;
             }
-            $return_sentence = implode(" ", $return_sentence);
-            array_push($final_array, $count, $return_sentence);
-            return $final_array;
         }
     }
 //Random response sentence
