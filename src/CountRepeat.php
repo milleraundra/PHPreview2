c5c2bdfe22ca6c7caff143aa28b3f1352fbce8a4
Aundra Miller <miller.aundra@gmail.com>
2016-02-19 08:23:25 -0800

    add initial project files and begin the first test

---
+++ src/CountRepeat.php
@@ -0,0 +1,18 @@
+<?php
+
+    class CountRepeat {
+
+        private $sentence;
+        private $word;
+
+        function trackCount($input_sentence, $input_word)
+        {
+            // $sentence_split = explode(" ", )
+            $count = 0;
+
+        }
+    }
+
+
+
+?>
---
+++ tests/CountRepeatTest.php
@@ -0,0 +1,36 @@
+<?php
+
+    require_once __DIR__."/../src/CountRepeat.php";
+
+    class CountRepeatTest extends PHPUnit_Framework_TestCase;
+    {
+        function test_oneWord()
+        {
+            $new_CountRepeat = new CountRepeat;
+            $input_sentence = "Mary";
+            $input_word = "mary";
+
+            $result = $new_CountRepeat->trackCount($input_sentence, $input_word);
+
+            $this->assertEquals(1, $result);
+        }
+
+        // function test_noMatch()
+        // {
+        //     $new_CountRepeat = new CountRepeat;
+        //     $input_sentence = "Mary Poppins";
+        //     $input_word = "Pop";
+        //
+        //     $result = $new_CountRepeat->trackCount($input_sentence, $input_word);
+        //
+        //     $this->assertEquals(0, $result);
+        // }
+    }
+
+
+
+
+
+
+
+ ?>
