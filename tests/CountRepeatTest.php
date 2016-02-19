475559155ad2f8c6e9c2bc82d23938abcb8b94e0
Aundra Miller <miller.aundra@gmail.com>
2016-02-19 08:40:23 -0800

    add and pass two more tests for inputs with no matches and multiple matches

--- tests/CountRepeatTest.php
+++ tests/CountRepeatTest.php
@@ -15,16 +15,27 @@
             $this->assertEquals(1, $result);
         }
 
-        // function test_noMatch()
-        // {
-        //     $new_CountRepeat = new CountRepeat;
-        //     $input_sentence = "Mary Poppins";
-        //     $input_word = "Pop";
-        //
-        //     $result = $new_CountRepeat->trackCount($input_sentence, $input_word);
-        //
-        //     $this->assertEquals(0, $result);
-        // }
+        function test_noMatch()
+        {
+            $new_CountRepeat = new CountRepeat;
+            $input_sentence = "Mary Poppins";
+            $input_word = "Pop";
+
+            $result = $new_CountRepeat->trackCount($input_sentence, $input_word);
+
+            $this->assertEquals(0, $result);
+        }
+
+        function test_multipleWords()
+        {
+            $new_CountRepeat = new CountRepeat;
+            $input_sentence = "To don't have to be great to start, but you have to be start to be great";
+            $input_word = "to";
+
+            $result = $new_CountRepeat->trackCount($input_sentence, $input_word);
+
+            $this->assertEquals(5, $result);
+        }
     }
 
 
